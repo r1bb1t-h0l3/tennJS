7b5369098c822588b8c5bd39a8f834e3c8671624
sasha <arefieva.aleksandra@gmail.com>
2024-10-28 11:57:56 +0100

    completed basic game code

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-//Setting up the canvas area
+    //Setting up the canvas area
 
 let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
 let width = canvas.width;
 let height = canvas.height;
 
+//Defining dfault speed for computer
+const MAX_COMPUTER_SPEED = 2;
 
 // Defining the draw function with ball and background info
 const BALL_SIZE = 5;
-let ballPosition = {x: 20, y: 30};
+let ballPosition;
 
-let xSpeed = 4;
-let ySpeed = 2;
+let xSpeed;
+let ySpeed;
+
+function initBall() {
+    ballPosition = {x: 20, y: 30};
+    xSpeed = 4;
+    ySpeed = 2;
+}
 
 const PADDLE_WIDTH = 5;
 const PADDLE_HEIGHT = 20;
@@ -19,6 +27,11 @@ const PADDLE_OFFSET = 10;
 
 let leftPaddleTop = 10;
 let rightPaddleTop = 30;
+
+let leftScore = 0;
+let rightScore = 0;
+let gameOver = false;
+
 
 //adding event listener to move left paddle
 document.addEventListener("mousemove", e => {
@@ -49,11 +62,59 @@ function draw() {
         PADDLE_WIDTH,
         PADDLE_HEIGHT
     );
+
+    //List scores
+
+    ctx.font = "30px monospace";
+    ctx.textAlign = "left";
+    ctx.fillText(leftScore.toString(), 50, 50);
+    ctx.textAlign = "right";
+    ctx.fillText(rightScore.toString(), width - 50, 50);
+}
+
+function followBall() {
+    let ball = {
+        top: ballPosition.y,
+        bottom: ballPosition.y + BALL_SIZE
+    };
+
+    let leftPaddle = {
+        top: leftPaddleTop,
+        bottom: leftPaddleTop + PADDLE_HEIGHT
+    };
+
+    if (ball.top < leftPaddle.top) {
+        leftPaddleTop -= MAX_COMPUTER_SPEED;
+    } else if (ball.bottom > leftPaddle.bottom) {
+        leftPaddleTop += MAX_COMPUTER_SPEED;
+    }
 }
 
 function update() {
     ballPosition.x += xSpeed;
     ballPosition.y += ySpeed;
+    followBall();
+}
+
+function checkPaddleCollision(ball, paddle) {
+    //Check if the paddle and ball overlap vertically and horizontally
+
+    return (
+        ball.left < paddle.right &&
+        ball.right > paddle.left &&
+        ball.top < paddle.bottom &&
+        ball.bottom > paddle.top
+    );
+}
+
+function adjustAngle(distanceFromTop, distanceFromBottom) {
+    if (distanceFromTop < 0) {
+        //If ball hit near top of paddle, reduce ySpeed
+        ySpeed -= 0.5;
+    } else if (distanceFromBottom < 0) {
+        //If ball hit near bottom of paddle, increase ySpeed
+        ySpeed += 0.5;
+    }
 }
 
 function checkCollision() {
@@ -64,8 +125,48 @@ function checkCollision() {
         bottom: ballPosition.y + BALL_SIZE
     }
 
-    if (ball.left < 0 || ball.right > width) {
-        xSpeed = -xSpeed;
+    let leftPaddle = {
+        left: PADDLE_OFFSET,
+        right: PADDLE_OFFSET + PADDLE_WIDTH,
+        top: leftPaddleTop,
+        bottom: leftPaddleTop + PADDLE_HEIGHT
+    };
+
+    let rightPaddle = {
+        left: width - PADDLE_WIDTH - PADDLE_OFFSET,
+        right: width - PADDLE_OFFSET,
+        top: rightPaddleTop,
+        bottom: rightPaddleTop + PADDLE_HEIGHT
+    };
+
+    if (checkPaddleCollision(ball, leftPaddle)) {
+        //Left paddle collision happened
+        let distanceFromTop = ball.top - leftPaddle.top;
+        let distanceFromBottom = leftPaddle.bottom - ball.bottom;
+        adjustAngle(distanceFromTop, distanceFromBottom);
+        xSpeed = Math.abs(xSpeed);
+    }
+
+    if (checkPaddleCollision(ball, rightPaddle)) {
+        //Right paddle collision happened
+        let distanceFromTop = ball.top - rightPaddle.top;
+        let distanceFromBottom = rightPaddle.bottom - ball.bottom;
+        adjustAngle(distanceFromTop, distanceFromBottom);
+        xSpeed = -Math.abs(xSpeed);
+    }
+
+    if (ball.left < 0) {
+        rightScore++;
+        initBall();
+    }
+
+    if (ball.right > width) {
+        leftScore++;
+        initBall();
+    }
+
+    if (leftScore > 9 || rightScore > 9) {
+        gameOver = true;
     }
 
     if (ball.top < 0 || ball.bottom > height) {
@@ -73,14 +174,26 @@ function checkCollision() {
     }
 }
 
+function drawGameOver() {
+    ctx.fillStyle = "white";
+    ctx.font = "30px monospace";
+    ctx.textAlign = "center";
+    ctx.fillText("GAME OVER", width / 2, height / 2);
+}
+
 
 function gameLoop() {
     draw();
     update();
     checkCollision();
-
+    if (gameOver) {
+        draw();
+        drawGameOver();
+    } else {
     //Call this function again after a timeout
     setTimeout(gameLoop, 30);
+    }
 }
 
+initBall();
 gameLoop();
